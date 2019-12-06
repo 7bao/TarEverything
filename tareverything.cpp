8f31e4fecca06df78b45e28bd89cd14c43ed39d2
tony <qgruan@outlook.com>
2019-12-06 10:34:38 +0800

    rewrite main function

--- tareverything.cpp
+++ tareverything.cpp
@@ -1,14 +1,26 @@
 #include "tareverything.h"
 
 int main(int argc ,char **argv){
-    if(argc != 2) {
-        cout << "No such file or dirctory" << endl;     
-        exit(1);
-    }
-    string arg_1 = argv[1];
+    string arg_1;
+    string arg_2;
     Tareverything tar;
-    tar.write_para(arg_1); 
-    cout << tar.command << endl;
-    tar.tar_command(tar.command);
+    switch(argc){
+        case tar.sing_suff:
+            arg_1 = argv[1];
+            tar.write_para(arg_1); 
+            cout << tar.command << endl;
+            tar.tar_command(tar.command);
+            break;
+        case tar.doub_suff:
+            arg_1 = argv[1];
+            arg_2 = argv[2];
+            tar.write_para(arg_1,arg_2); 
+            cout << tar.command << endl;
+            tar.tar_command(tar.command);
+            break;
+        default:
+            cout << "No such file or dirctory" << endl;     
+            exit(1);
+    }
 }
 
